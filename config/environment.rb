8857fc7d7ac9641c3d7cc92db9dd9eb007e062fd
Tim Quimbo <tquimbo1@gmail.com>
2021-05-05 18:45:32 -0400

    commit tree fecfe0857630f3f2d86bdbbb12383a2d21758c46

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,19 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require './app'
 require_all 'models'
+
+class App < Sinatra::Base
+
+    get '/name' do
+    "My name is Tim."
+    end
+
+    get '/hometown' do
+        "My hometown is Woodcliff Lake."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Waves."
+    end
+
+end
